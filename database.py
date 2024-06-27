a77c8ab81e382f68c2742a3770529b23de2397fc
Sadman <your-email@example.com>
2024-06-26 21:38:51 -0400

    Updating ui and database files

--- database.py
+++ database.py
@@ -1,4 +1,26 @@
 # This file handles the database connection and queries 
+
 import psycopg2
 from psycopg2 import sql
+
+def connect_to_db():
+    conn = psycopg2.connect(
+        dbname='deds', #TODO: change this to the name of the database
+        user = 'postgres',
+        password = 'password', #TODO: change this to the password of the database
+        host = 'localhost',
+        port = '5432'
+    )
+    return conn
+
+def view_fighters(conn):
+    cur = conn.cursor() # create a cursor object using the connection
+    cur.execute("SELECT * FROM fighters") # execute the query
+    rows = cur.fetchall() # fetch all the rows
+    return rows
+
+def close_db(conn):
+    conn.close() # close the connection
+
+
 
---
+++ ui.py
@@ -0,0 +1,4 @@
+# This file is the user interface for the application
+
+from prettytable import PrettyTable
+import database
